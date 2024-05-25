92c9705fda47db33a4c4c5d7c076b74f129baedd
Greg Teixeira <greg.teixeira123@gmail.com>
2024-05-24 22:11:04 -0300

    feat: add tailwind css 4

--- src/App.tsx
+++ src/App.tsx
@@ -17,17 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="p-8">
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="text-gray-400">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
